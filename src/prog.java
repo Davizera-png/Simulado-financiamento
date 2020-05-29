cea7b0e6bbb6e21734ce085432ba4ecb848eb3c3
Davizera-png <olidavidson34@gmail.com>
2020-05-29 17:22:36 -0300

    Processamento do menu

--- src/prog.java
+++ src/prog.java
@@ -4,8 +4,8 @@ public class prog {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner sc = new Scanner(System.in);
-		int prest;	
-		double sal, entrada, totalf, aux, valprest;
+		int prest, opcao, entrada;	
+		double sal, totalf, aux, valprest, valentra, valfin, prestfinal;
 		String nome;
 
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
@@ -22,12 +22,12 @@ Scanner sc = new Scanner(System.in);
 	    System.out.println("Digite o número de prestações:");
 	    prest = sc.nextInt();
 	    System.out.println("Porcentagem da entrada:");
-	    entrada = sc.nextDouble();
+	    entrada = sc.nextInt();
 	    System.out.println("Volor total financiado:");
 	    totalf = sc.nextDouble();
 	    valprest = totalf / prest;
-	    aux =  30 * sal / 100;
-	    while (valprest > aux) {
+	    aux = totalf * (100 - entrada) / 100.0 / prest ;
+	    while (aux > 0.3 * sal) {
 	    	System.out.println("Valor da prestação excede 30% do salário, insira novos valores");
 	    	System.out.println("Salario:");
 			sal = sc.nextDouble();
@@ -40,12 +40,38 @@ Scanner sc = new Scanner(System.in);
 		    System.out.println("Digite o número de prestações:");
 		    prest = sc.nextInt();
 		    System.out.println("Porcentagem da entrada:");
-		    entrada = sc.nextDouble();
+		    entrada = sc.nextInt();
 		    System.out.println("Volor total financiado:");
 		    totalf = sc.nextDouble();
-		    aux =  30 * sal / 100;
+		    aux = totalf * (100 - entrada) / 100.0 / prest ;
 	    }
 	    // fim valor prestações
+	    //processo menu
+	    valentra = entrada * totalf / 100;
+	    valfin = totalf - entrada;
+	    prestfinal = totalf - valentra / prest;
+	     do {
+	    	 System.out.println("Menu: ");
+		    	System.out.println("1 - Mostrar o valor da entrada");
+		    	System.out.println("2 - Mostrar o valor finnanciado");
+		    	System.out.println("3 - Mostrar o valor de cada prestação");
+		    	System.out.println("4 - Sair");
+		    	opcao = sc.nextInt();
+		    	if (opcao < 0 && opcao > 4) {
+		    		System.out.println("Opção inválida, insira uma opção válida");
+		    		} else if (opcao == 1) {
+		    						    		System.out.println("ENTRADA = R$ "+ valentra);
+			    		
+			    		} else if (opcao == 2) {
+				    		System.out.println("VALOR FINANCIADO = R$ "+ valfin);
+				    		} else if (opcao == 3) {
+					    		System.out.println("VALOR DE CADA PRESTAÇÃO = R$ "+ prestfinal);
+					    		}
+		    	
+	     }
+	    while (opcao != 4);
+
+	    
 		sc.close();
 	}
 
