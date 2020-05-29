42c8b5b90cca5b9c208a4d03b810d9083f65e479
Davizera-png <olidavidson34@gmail.com>
2020-05-29 16:06:31 -0300

    Leitura do financiamento

--- src/prog.java
+++ src/prog.java
@@ -4,20 +4,48 @@ public class prog {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner sc = new Scanner(System.in);
-		float sal;
+		int prest;	
+		double sal, entrada, totalf, aux, valprest;
 		String nome;
 
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.println("Nome do cliente:");
 		nome = sc.nextLine();
 		System.out.println("Salario:");
-		sal = sc.nextFloat();
+		sal = sc.nextDouble();
 		
 		while (sal < 0) {
 			System.out.println("Salário inválido, digite outro valor");
-			sal = sc.nextFloat();}
-		
-		
+			sal = sc.nextFloat();
+			}
+		//valor do numero de prestações
+	    System.out.println("Digite o número de prestações:");
+	    prest = sc.nextInt();
+	    System.out.println("Porcentagem da entrada:");
+	    entrada = sc.nextDouble();
+	    System.out.println("Volor total financiado:");
+	    totalf = sc.nextDouble();
+	    valprest = totalf / prest;
+	    aux =  30 * sal / 100;
+	    while (valprest > aux) {
+	    	System.out.println("Valor da prestação excede 30% do salário, insira novos valores");
+	    	System.out.println("Salario:");
+			sal = sc.nextDouble();
+			
+			while (sal < 0) {
+				System.out.println("Salário inválido, digite outro valor");
+				sal = sc.nextFloat();
+				}
+			//valor do numero de prestações
+		    System.out.println("Digite o número de prestações:");
+		    prest = sc.nextInt();
+		    System.out.println("Porcentagem da entrada:");
+		    entrada = sc.nextDouble();
+		    System.out.println("Volor total financiado:");
+		    totalf = sc.nextDouble();
+		    aux =  30 * sal / 100;
+	    }
+	    // fim valor prestações
 		sc.close();
 	}
 
