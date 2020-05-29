44b96e93e72e946092973192ffc4ecc3c56ccd13
Davizera-png <olidavidson34@gmail.com>
2020-05-29 15:27:39 -0300

    Leitura de nome e salário

--- src/prog.java
+++ src/prog.java
@@ -1,9 +1,24 @@
-
+import java.util.Scanner;
 public class prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+		float sal;
+		String nome;
 
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.println("Nome do cliente:");
+		nome = sc.nextLine();
+		System.out.println("Salario:");
+		sal = sc.nextFloat();
+		
+		while (sal < 0) {
+			System.out.println("Salário inválido, digite outro valor");
+			sal = sc.nextFloat();}
+		
+		
+		sc.close();
 	}
 
 }
